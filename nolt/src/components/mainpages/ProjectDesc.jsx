64c6c25429be440aab0e1cad0b7aa5c9ede75f4f
schaldach <gabimorgado0311@gmail.com>
2022-06-05 20:30:51 -0300

    Update ProjectDesc.jsx

--- nolt/src/components/mainpages/ProjectDesc.jsx
+++ nolt/src/components/mainpages/ProjectDesc.jsx
@@ -1,10 +1,16 @@
 import React from "react"
 import SecondTitle from "./smallcomponents/SecondTitle"
+import SectionDescription from "./smallcomponents/SectionDescription";
 
 function ProjectDesc({visualclass}) {
+    function textToWrite(){
+        return 'O projeto Nolt baseado em um antigo projeto Lead, que tem como principal objetivo ajudar a organizar notas, listas e links pessoais, trazendo todos estes elementos em um lugar só.'
+    }
+
     return (
         <div className={visualclass}>
             <SecondTitle titlecontent='Projeto'/>
+            <SectionDescription paragraph={textToWrite()}/>
         </div>
     )
 }
