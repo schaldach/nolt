05f79f9314e056bc271737cad8302b36ea3b17bd
schaldach <gabimorgado0311@gmail.com>
2022-06-06 21:17:06 -0300

    animação no css delicinha

--- nolt/src/components/mainpages/notetypes/smallercomponents/DropdownMenu.jsx
+++ nolt/src/components/mainpages/notetypes/smallercomponents/DropdownMenu.jsx
@@ -2,16 +2,14 @@ import React from "react";
 
 function DropdownMenu({editMode, startEdit, viewMode, startView, onDelete}) {
     return (
-        <div className='functionality dropdown-6'>
+        <div className='functionality'>
             <button 
             className='dropdownbutton functionbutton'></button>
-            <div className='dropmenu'>
-                <button onClick={() => startEdit(!editMode)} 
-                className={editMode?' greenbutton editimg functionbutton':'editimg functionbutton'}></button>
-                <button onClick={() => startView(!viewMode)} 
-                className={viewMode?' greenbutton viewimg functionbutton':'viewimg functionbutton'}></button>
-                <button onClick={onDelete} className='deletebutton functionbutton'>x</button>
-            </div>
+            <button onClick={() => startEdit(!editMode)} 
+            className={editMode?' greenbutton editimg functionbutton':'editimg functionbutton'}></button>
+            <button onClick={() => startView(!viewMode)} 
+            className={viewMode?' greenbutton viewimg functionbutton':'viewimg functionbutton'}></button>
+            <button onClick={onDelete} className='deletebutton functionbutton'>x</button>
         </div>
     )
 }
