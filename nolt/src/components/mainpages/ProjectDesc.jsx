58ac51f545e1a3fd7faf5e603ee9dca999bd5f22
schaldach <gabimorgado0311@gmail.com>
2022-06-20 22:23:05 -0300

    Update ProjectDesc.jsx

--- nolt/src/components/mainpages/ProjectDesc.jsx
+++ nolt/src/components/mainpages/ProjectDesc.jsx
@@ -7,8 +7,8 @@ function ProjectDesc({visualclass}) {
         <div className={visualclass}>
             <SecondTitle titlecontent='Projeto'/>
             <div className="secondtext">
-            Este projeto é baseado em um antigo projeto Lead, que tem como principal objetivo ajudar a 
-            organizar notas, listas e links pessoais, trazendo todos estes elementos em um lugar só.<br/><br/> 
+            Este projeto é baseado em um antigo projeto Lead, e tem como principal objetivo ajudar a 
+            organizar todas as notas, listas e links pessoais, trazendo todos estes elementos em um lugar só.<br/><br/> 
             Desta forma, nunca foi tão fácil organizar as pequenas e grandes coisas da sua vida pessoal.
             </div>
         </div>
