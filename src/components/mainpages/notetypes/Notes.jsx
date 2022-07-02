d2b0cf50aabc2703d078c48215d965020c875eec
schaldach <gabimorgado0311@gmail.com>
2022-07-01 21:44:39 -0300

    bonito demaiss

--- src/components/mainpages/notetypes/Notes.jsx
+++ src/components/mainpages/notetypes/Notes.jsx
@@ -27,7 +27,7 @@ function Notes({ visualnote, onNoteAdded, onNoteRemoved }) {
         console.log(newNotes)
         const bla = await supabase
             .from('notas')
-            .upsert(newNotes, oldNotes)
+            .upsert([...newNotes, ...oldNotes])
             .then( async () => {
                 const { data, count } = await supabase
                     .from('notas')
@@ -45,6 +45,7 @@ function Notes({ visualnote, onNoteAdded, onNoteRemoved }) {
         let newNote = { title: '', content: '', id: Math.floor(Math.random() * 1000000000), isNew:true}
         addNote([...allNotes, newNote])
         onNoteAdded('notas', 1)
+        conectionMade(1)
     }
 
     function onEdit(title, content, note) {
@@ -98,9 +99,10 @@ function Notes({ visualnote, onNoteAdded, onNoteRemoved }) {
                         Os dados não estão sincronizados.
                     </div>
                     <div className={sucessAnimation==2?'status':'displaynone'}>
-                        <svg xmlns="http://www.w3.org/2000/svg" className='conectionsvg' fill="none" viewBox="0 0 24 24" stroke="var(--color1)" strokeWidth={2}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className='conectionsvg rotating' fill="none" viewBox="0 0 24 24" stroke="var(--color1)" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
                         </svg>
+                        Atualizando...
                     </div>
                 </div>
             </div>
