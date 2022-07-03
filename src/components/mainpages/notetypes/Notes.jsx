ca47b07f40c9519723687fdd233c36f036324979
schaldach <gabimorgado0311@gmail.com>
2022-07-03 17:53:11 -0300

    pequeno bug

--- src/components/mainpages/notetypes/Notes.jsx
+++ src/components/mainpages/notetypes/Notes.jsx
@@ -69,6 +69,7 @@ function Notes({ visualnote, onNoteAdded, onNoteRemoved }) {
     }
 
     async function onDelete(noteId) {
+        let lastconection = sucessAnimation
         conectionMade(2)
         let newNotes = allNotes.filter(notes => notes.id !== noteId)
         const eba = await supabase
@@ -76,7 +77,7 @@ function Notes({ visualnote, onNoteAdded, onNoteRemoved }) {
             .delete()
             .match({ id: noteId })
             .then( () => {
-                conectionMade(0)
+                conectionMade(lastconection)
             })
         onNoteRemoved()
         addNote(newNotes)
@@ -94,19 +95,19 @@ function Notes({ visualnote, onNoteAdded, onNoteRemoved }) {
                     <button className='savebutton' onClick={pulseAnimation}>Salvar Notas</button>
                 </div>
                 <div>
-                    <div className={sucessAnimation==0?'status':'displaynone'}>
+                    <div className={sucessAnimation===0?'status':'displaynone'}>
                         <svg xmlns="http://www.w3.org/2000/svg" className='conectionsvg' fill="none" viewBox="0 0 24 24" stroke="#2e856e" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
                         </svg>
                         Os dados estão sincronizados.
                     </div>
-                    <div className={sucessAnimation==1?'status':'displaynone'}>
+                    <div className={sucessAnimation===1?'status':'displaynone'}>
                         <svg xmlns="http://www.w3.org/2000/svg" className='conectionsvg' fill="none" viewBox="0 0 24 24" stroke="#e61e1e" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                         </svg>
                         Os dados não estão sincronizados.
                     </div>
-                    <div className={sucessAnimation==2?'status':'displaynone'}>
+                    <div className={sucessAnimation===2?'status':'displaynone'}>
                         <svg xmlns="http://www.w3.org/2000/svg" className='conectionsvg rotating' fill="none" viewBox="0 0 24 24" stroke="var(--color1)" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
                         </svg>
