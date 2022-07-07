d24911df31edeca8542abfff6df5828c66606023
schaldach <gabimorgado0311@gmail.com>
2022-07-06 23:00:49 -0300

    realmente bonito

--- src/components/mainpages/notetypes/smallercomponents/DropdownMenu.jsx
+++ src/components/mainpages/notetypes/smallercomponents/DropdownMenu.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 function DropdownMenu({editMode, onEdit, viewMode, onView, onDelete, onFavorite, favorite}) {
     return (
+        <>
         <div className={viewMode ? 'functionality menuview' : 'functionality'}>
             <button className='dropdownbutton functionbutton'>
                 <svg xmlns="http://www.w3.org/2000/svg" className="dropdownsvg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
@@ -14,13 +15,13 @@ function DropdownMenu({editMode, onEdit, viewMode, onView, onDelete, onFavorite,
                 </svg>
             </button>
             <button onClick={onView} className='functionbutton'>
-                <svg xmlns="http://www.w3.org/2000/svg" className='dropdownsvg' fill="none" viewBox="0 0 24 24" stroke={viewMode ? 'var(--color3)' : 'currentColor'} strokeWidth={2}>
+                <svg xmlns="http://www.w3.org/2000/svg" className='dropdownsvg' fill='none' viewBox="0 0 24 24" stroke={viewMode ? 'var(--color3)' : 'currentColor'} strokeWidth={2}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                 </svg>
             </button>
             <button onClick={onFavorite} className="functionbutton">
-                <svg xmlns="http://www.w3.org/2000/svg" className="dropdownsvg" fill={favorite?'var(--color3)':'none'} viewBox="0 0 24 24" stroke={favorite?'var(--color3)':'none'} strokeWidth={2}>
+                <svg xmlns="http://www.w3.org/2000/svg" className="dropdownsvg" fill='none' viewBox="0 0 24 24" stroke={favorite?'var(--color3)':'currentColor'} strokeWidth={2}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
                 </svg>
             </button>
@@ -29,7 +30,8 @@ function DropdownMenu({editMode, onEdit, viewMode, onView, onDelete, onFavorite,
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                 </svg>
             </button>
-        </div>
+        </div>     
+        </>
     )
 }
 
