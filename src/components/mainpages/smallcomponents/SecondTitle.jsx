3cc6882f6a82028207fe247449a433662f140cde
schaldach <gabimorgado0311@gmail.com>
2022-07-23 20:07:57 -0300

    instalando next

--- src/components/mainpages/smallcomponents/SecondTitle.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function SecondTitle({titlecontent, extra}) {
-    return(
-        <div className='titlesecond'>{titlecontent}<span className='coloredtext'>{extra}</span></div>
-    )
-}
-
-export default SecondTitle;
