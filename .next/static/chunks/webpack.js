e081061b65f1fbade610e37db1361f8f243e80f8
schaldach <gabimorgado0311@gmail.com>
2022-07-27 10:51:51 -0300

    css do menu

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dcd9e23865d84108"; }
+/******/ 		__webpack_require__.h = function() { return "4e39371bf694699b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
