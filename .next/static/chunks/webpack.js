bb641a8f5acccdc7e09057254af77fc56578555e
schaldach <gabimorgado0311@gmail.com>
2022-07-27 13:02:10 -0300

    500 ALTERAÇÃO KRL

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e9845fecf58b0218"; }
+/******/ 		__webpack_require__.h = function() { return "df27509384a7e554"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
