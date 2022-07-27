a637ee10d9ecbd73e3bc66cc70ffc317f8faf205
schaldach <gabimorgado0311@gmail.com>
2022-07-27 01:19:45 -0300

    pequenas alterações

--- pages/project.js
+++ pages/project.js
@@ -6,9 +6,9 @@ function Project() {
             <SecondTitle titlecontent='Projeto'/>
             <div className="secondtext">
             Este projeto é baseado em um antigo projeto Lead, e tem como principal objetivo ajudar a 
-            organizar todas as notas, listas e links pessoais, trazendo todos estes elementos em um lugar só.<br/><br/> 
+            organizar todas as notas, listas e links pessoais, trazendo todos estes elementos em um lugar só, salvo na Internet.<br/><br/> 
             Desta forma, nunca foi tão fácil organizar as pequenas e grandes coisas da sua vida pessoal.<br/><br/>
-            Desenvolvido por Gabriel Schaldach Morgado com React.js e Supabase.
+            Desenvolvido por Gabriel Schaldach Morgado com React.js e Supabase, e reconstruído com Next.js.
             </div>
         </div>
     )
