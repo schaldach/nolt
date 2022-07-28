e96ec6c531450a38a3ce0bf98a991d3b0263160c
schaldach <gabimorgado0311@gmail.com>
2022-07-28 00:53:12 -0300

    svgs de login e logout

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c641fda4f4b370d8"; }
+/******/ 		__webpack_require__.h = function() { return "475a6d573a046138"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/auth.js
+++ pages/auth.js
@@ -57,8 +57,16 @@ function Auth({throwError, reqlog, errorMessage}) {
                         </svg>
                     </button>
                 </div>
-                <button className={login?"loginbutton logintext":'displaynone'} onClick={signin}>Fazer Login</button>
-                <button className={login?'displaynone':"loginbutton logintext"} onClick={signup}>Fazer Cadastro</button>
+                <button className={login?"loginbutton logintext":'displaynone'} onClick={signin}>Login
+                <svg xmlns="http://www.w3.org/2000/svg" className="loginsvg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
+                </svg>
+                </button>
+                <button className={login?'displaynone':"loginbutton logintext"} onClick={signup}>Cadastro
+                <svg xmlns="http://www.w3.org/2000/svg" className="loginsvg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
+                </svg>
+                </button>
                 <div className="loginerror logintext">{errortext()}</div>
                 <div className="loginchangewrapper">{login?'Não possui uma conta?':'Já tem uma conta?'}
                     <button className="loginchange" onClick={() => changeMode(!login)}>{login?'Cadastro':'Login'}</button>                
