d3bcd1821432889e366c03da04479afaf85e4284
schaldach <gabimorgado0311@gmail.com>
2022-07-29 13:20:00 -0300

    svg de lixeira

--- components/LinkAnotation.js
+++ components/LinkAnotation.js
@@ -18,7 +18,7 @@ function LinkAnotation({linkcontent, linkname, onDelete, id, favorite, onFavorit
             <div className="outermenu">
                 <button onClick={() => setBox(true)} className='deletelinkbutton'>
                     <svg xmlns="http://www.w3.org/2000/svg" className="dropdownsvg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                     </svg>
                 </button>
                 <button onClick={() => onFavorite(link)} className='favoritelink'>
