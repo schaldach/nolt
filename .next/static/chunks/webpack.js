a052ebf296f8090bc5b0708d3f9387e122cb8a54
schaldach <gabimorgado0311@gmail.com>
2022-07-29 17:28:40 -0300

    ok

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c71e020bf2c605a0"; }
+/******/ 		__webpack_require__.h = function() { return "d40f5b0e6c36f989"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
