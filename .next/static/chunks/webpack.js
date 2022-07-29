075ed373d9cc6997b62e4bf2da1c47745b5dd70b
schaldach <gabimorgado0311@gmail.com>
2022-07-28 22:08:27 -0300

    balao de info para a sincronização

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3cb5b2f0db9ac9f3"; }
+/******/ 		__webpack_require__.h = function() { return "bf6a77312674775b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/listas.js
+++ pages/listas.js
@@ -109,6 +109,13 @@ function Lists({user, reqsync}) {
     return (
         <div>
             <SecondTitle titlecontent='Anotações' extra='/Listas'/>
+            <button className='infobutton'>
+                <svg xmlns="http://www.w3.org/2000/svg" className='navbarsvg' viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
+                </svg>
+                <div className='infobox'>Não saia da página caso as anotações não estejam sincronizadas. Elas podem ser salvas manualmente ou automaticamente, 
+                que acontece a cada 20 segundos caso hajam alterações.</div>
+            </button>
             <div className='flex'>
                 <div className={animation ? 'panimation wrapdiv' : 'wrapdiv'} onAnimationEnd={() => startAnimation(false)}>
                     <button className='savebutton' onClick={pulseAnimation}>Salvar Listas</button>
