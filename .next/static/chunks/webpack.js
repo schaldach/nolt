1b996873d78bbcece391f04692bdfc74bd03e9b1
schaldach <gabimorgado0311@gmail.com>
2022-07-29 16:54:15 -0300

    foi por favor!

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "30344d7fe7574f34"; }
+/******/ 		__webpack_require__.h = function() { return "ecd21c3863853d12"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
