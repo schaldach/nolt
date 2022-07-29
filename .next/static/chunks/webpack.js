64f40c66b4d869690b10717efc027b40fb7d4512
schaldach <gabimorgado0311@gmail.com>
2022-07-29 16:16:56 -0300

    margin do botao de salvar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f7a0fe6eb258fa25"; }
+/******/ 		__webpack_require__.h = function() { return "2b8683f02b6feb78"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
