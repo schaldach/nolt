0a2a85dc05f891fb4a2defa68ac61da9dcceaf13
schaldach <gabimorgado0311@gmail.com>
2022-08-01 21:54:01 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -49,6 +49,7 @@ function Project({setProject, user}) {
                     <svg xmlns="http://www.w3.org/2000/svg" className="landingsvg blacktext" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M19 13l-7 7-7-7m14-8l-7 7-7-7" />
                     </svg>
+                    <div className="landingtext blacktext">Principais features</div>
                     <div className="landingflex">
                         <div className="landingtext landinghalf minor blacktext">Os seus dados ficarão salvos na sua conta para que você possa acessá-los em qualquer dispositivo
                         que também esteja logado. 
