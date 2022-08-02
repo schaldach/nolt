89701d24068477800fdea23b3731e27aaad9a7de
schaldach <gabimorgado0311@gmail.com>
2022-08-02 16:39:57 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ function Project({setProject, user}) {
                 <svg xmlns="http://www.w3.org/2000/svg" className="landingsvg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M19 13l-7 7-7-7m14-8l-7 7-7-7" />
                 </svg>
-                <div className="landingtext">Como funciona</div>
+                <div className="landingtext">Como funciona?</div>
                 <div className="landingtext minor">Após criar sua conta, você terá acesso ao site, onde
                 poderá fazer anotações.<br/> Elas incluem notas, listas, links e grupos, com o objetivo de
                 ajudar na sua organização pessoal.
