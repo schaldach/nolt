28e243249ef5dbbf83ac4080d72f6ffb9b0fe728
schaldach <gabimorgado0311@gmail.com>
2022-08-01 21:44:35 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -67,7 +67,7 @@ function Project({setProject, user}) {
                     </div>
                     <div className="landingflex">
                         <div className="landingtext landinghalf minor blacktext">Os seus dados estão protegidos com a política de Row Level Security,
-                        oferecida pelo Supabase para fornecer privacidade à sua conta pessoal.
+                        que fornece privacidade e segurança para os seus dados.
                         </div>
                         <svg xmlns="http://www.w3.org/2000/svg" className="landingsvg blacktext" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
