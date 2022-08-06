cdb6510c5c22ef88197b784aefd6d49cdda83272
schaldach <gabimorgado0311@gmail.com>
2022-08-06 16:16:19 -0300

    simples

--- components/SimpleLink.js
+++ components/SimpleLink.js
@@ -1,6 +1,6 @@
 function SimpleLink({href, name}) {
     return (
-        <div className='littlelink wholeborder'>
+        <div className='littlelink wholeborder favlink'>
             <img className='linkimage' src={`http://www.google.com/s2/favicons?sz=32&domain_url=${href}`} />
             <a className='linktext' href={href} target='_blank' rel='noreferrer'>{name}</a>
         </div>
