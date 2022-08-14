5c685c34a9bf7f79be8ede420ff628f63bd83c63
schaldach <gabimorgado0311@gmail.com>
2022-08-13 23:53:11 -0300

    ok

--- pages/grupos.js
+++ pages/grupos.js
@@ -119,7 +119,7 @@ function Groups({user}) {
             </div>
             <div className="displaygroups">
                 <button className="addanotation" onClick={addGroup}>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="connectionsvg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
                     </svg>
                 </button>
--- pages/links.js
+++ pages/links.js
@@ -80,7 +80,7 @@ function Links({user}) {
             </div>
             <div className='displayanotations'>
                 <button className='addanotation' onClick={() => requestD(true)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="connectionsvg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
                     </svg>
                 </button>
