6ccc1ca95d2121fcb2b0af113825ffc70ffcad55
schaldach <gabimorgado0311@gmail.com>
2022-08-14 21:11:32 -0300

    Revert "codigo redundante"
    This reverts commit 7e3fde8ca3fcba00e966648cb799f6a7ff410837.

--- components/PreviewLinks.js
+++ components/PreviewLinks.js
@@ -5,7 +5,7 @@ function PreviewLinks({addLink, links, allLinks, showLinks, linksBox, type}) {
     return (
         <PreviewBox type={type} isShown={linksBox} exit={() => showLinks(false)}>
             {allLinks.map(link =>
-                <div key={link.id} onClick={() => addLink(link)} className="previewnote favanot">
+                <div key={link.id} onClick={() => addLink(link)} className={links.indexOf(link)!==-1?"previewnote checkpreview":'previewnote'}>
                 <SimpleLink name={link.name} href={link.href}></SimpleLink>
                 <svg xmlns="http://www.w3.org/2000/svg" className={links.indexOf(link)!==-1?'previewcheckedsvglink':'displaynone'} fill="none" viewBox="0 0 24 24" stroke="var(--color5)" strokeWidth={3}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
--- components/PreviewNotes.js
+++ components/PreviewNotes.js
@@ -5,7 +5,7 @@ function PreviewNotes({addNote, notes, allNotes, showNotes, notesBox, type}) {
     return (
         <PreviewBox type={type} isShown={notesBox} exit={() => showNotes(false)}>
             {allNotes.map(note =>
-                <div key={note.id} onClick={() => addNote(note)} className='previewnote favanot'>
+                <div key={note.id} onClick={() => addNote(note)} className={notes.indexOf(note)!==-1?"previewnote checkpreview":'previewnote'}>
                     <SimpleAnotation small={note.small} calendar={note.calendar} date={note.date} content={note.content} title={note.title}></SimpleAnotation>
                     <svg xmlns="http://www.w3.org/2000/svg" className={notes.indexOf(note)!==-1?'previewcheckedsvg':'displaynone'} fill="none" viewBox="0 0 24 24" stroke="var(--color5)" strokeWidth={2}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
