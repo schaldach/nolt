56c44e21c11b9b45e2349f6f293dc86346518e55
schaldach <gabimorgado0311@gmail.com>
2022-08-23 18:31:59 -0300

    absolute svg esta de volta

--- components/NavBar.js
+++ components/NavBar.js
@@ -30,6 +30,9 @@ function NavBar({darkMode, setDarkMode, currentNote, changeCurrentNote, currentP
                 <Link href={'/'+currentNote} >
                     <div className='contentselection' onClick={() => changeCurrentPage(currentNote)}>
                         <div className='navbar-select'>
+                        <svg xmlns="http://www.w3.org/2000/svg" className='absolutesvg' fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                        </svg>
                         <div className={currentNote==='notas'?'':'displaynone'}>
                             <svg xmlns="http://www.w3.org/2000/svg" className={currentPage==='notas'?"navbarsvg":"displaynone"} viewBox="0 0 24 24" fill="currentColor">
                                 <path fillRule="evenodd" d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0016.5 9h-1.875a1.875 1.875 0 01-1.875-1.875V5.25A3.75 3.75 0 009 1.5H5.625zM7.5 15a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 017.5 15zm.75 2.25a.75.75 0 000 1.5H12a.75.75 0 000-1.5H8.25z" clipRule="evenodd" />
