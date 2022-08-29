3ae9fab01eda6d9cfbbc5830f7bb96aebe9e5d62
schaldach <gabimorgado0311@gmail.com>
2022-08-29 20:42:41 -0300

    hover nos function buttons (acabou?)

--- components/DropdownMenu.js
+++ components/DropdownMenu.js
@@ -16,36 +16,36 @@ function DropdownMenu({activated, showDropdowns, editMode, onEdit, viewMode, onV
                 </svg>
             </button>
             }
-            <button onClick={onEdit} className='functionbutton'>
-                <svg xmlns="http://www.w3.org/2000/svg" className="dropdownsvg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke={editMode?'currentColor':'var(--color1)'}>
+            <button onClick={onEdit} className={editMode?'functionbutton':'functionbutton idlefunction'}>
+                <svg xmlns="http://www.w3.org/2000/svg" className="dropdownsvg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke='currentColor'>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                 </svg>
             </button>
-            <button onClick={onView} className='functionbutton'>
-                <svg xmlns="http://www.w3.org/2000/svg" className='dropdownsvg' fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke={viewMode?'currentColor':'var(--color1)'}>
+            <button onClick={onView} className={viewMode?'functionbutton':'functionbutton idlefunction'}>
+                <svg xmlns="http://www.w3.org/2000/svg" className='dropdownsvg' fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke='currentColor'>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
             </button>
-            <button onClick={onFavorite} className="functionbutton">
-                <svg xmlns="http://www.w3.org/2000/svg" className="dropdownsvg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke={favorite?'currentColor':'var(--color1)'}>
+            <button onClick={onFavorite} className={favorite?"functionbutton":'functionbutton idlefunction'}>
+                <svg xmlns="http://www.w3.org/2000/svg" className="dropdownsvg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke='currentColor'>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
                 </svg>
             </button>
             {noCalendar?
-            <button onClick={onFilter} className="functionbutton">
-                <svg xmlns="http://www.w3.org/2000/svg" className="dropdownsvg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke={filtered?'currentColor':'var(--color1)'}>
+            <button onClick={onFilter} className={filtered?"functionbutton":'functionbutton idlefunction'}>
+                <svg xmlns="http://www.w3.org/2000/svg" className="dropdownsvg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke='currentColor'>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z" />
                 </svg>
             </button>:
-            <button onClick={onCalendar} className="functionbutton">
-                <svg xmlns="http://www.w3.org/2000/svg" className="dropdownsvg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke={calendar?'currentColor':'var(--color1)'}>
+            <button onClick={onCalendar} className={calendar?"functionbutton":'functionbutton idlefunction'}>
+                <svg xmlns="http://www.w3.org/2000/svg" className="dropdownsvg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke='currentColor'>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
                 </svg>
             </button>
             }
-            <button onClick={onSmall} className="functionbutton">
-                <svg xmlns="http://www.w3.org/2000/svg" className="dropdownsvg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke={!small?'currentColor':'var(--color1)'}>
+            <button onClick={onSmall} className={!small?"functionbutton":'functionbutton idlefunction'}>
+                <svg xmlns="http://www.w3.org/2000/svg" className="dropdownsvg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke='currentColor'>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
                 </svg>
             </button>
--- components/NecessaryData.js
+++ components/NecessaryData.js
@@ -4,14 +4,20 @@ function NecessaryDataLink({visualclass, onFinish, requestD}) {
     const [linkName, changeLinkName] = useState('')
     const [linkContent, changeLinkContent] = useState('')
 
+    function wholeFinish(){
+        changeLinkName('')
+        changeLinkContent('')
+        onFinish(linkName, linkContent)
+    }
+
     return (
         <div className={visualclass+' littlelink requestdata'}>
-            <input type='text'
+            <input type='text' value={linkName} 
             onInput={e => changeLinkName(e.target.value)} placeholder='Nome do Link'/>
-            <input type='text' 
+            <input type='text' value={linkContent} 
             onInput={e => changeLinkContent(e.target.value)} placeholder='Link (URL)'/>
             <div className='bothbuttons'>
-                <button onClick={() => onFinish(linkName, linkContent)} className='confirmbuttons'>Salvar</button>
+                <button onClick={wholeFinish} className='confirmbuttons'>Salvar</button>
                 <button onClick={() => requestD(false)} className='confirmbuttons'>Cancelar</button>
             </div>
         </div>
