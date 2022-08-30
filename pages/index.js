ac460184a521932eafe145d5dc57b376a230afa5
schaldach <gabimorgado0311@gmail.com>
2022-08-30 14:01:38 -0300

    foi bom trabalhar com você

--- pages/index.js
+++ pages/index.js
@@ -108,6 +108,15 @@ function Project({setProject, user}) {
                         que você use seu tempo com o que importa.
                         </div>
                     </div>
+                    <div className="landingflex">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="landingsvg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+                        </svg>
+                        <div className="cardanotation">Organização</div>
+                        <div className="landingtext landinghalf minor blacktext">Controle as suas anotações através dos favoritos e grupos, para visualizar
+                        somente aquelas que você precisa no momento.
+                        </div>
+                    </div>
                     </div>
             </div>
             <div className="landingfooter">
