bdab0dee911da2c51f1097de57269714d172698c
schaldach <gabimorgado0311@gmail.com>
2022-09-03 15:07:38 -0300

    Update password-recovery.js

--- pages/password-recovery.js
+++ pages/password-recovery.js
@@ -1,26 +1,32 @@
 import {supabase} from "../utils/supabaseClient"
 import Router from "next/router";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function PasswordRecovery() {
     const [newpassword, alterpassword] = useState('')
+    const [passwordRequest, reqPas] = useState(false)
     const accessToken = Router.query.access_token;
 
-    async function finalChange(){
-        supabase.auth.api.updateUser(accessToken, { newpassword })
-            .then(() => {
-                Router.push("/auth");
-            })
-            .catch(() => {
-                notify.error("token-expirou");
-            });
-    }
+    useEffect(() => {
+        async function finalChange(){
+            if(passwordRequest){
+                supabase.auth.api.updateUser(accessToken, { newpassword })
+                .then(() => {
+                    Router.push("/auth");
+                })
+                .catch(() => {
+                    notify.error("token-expirou");
+                });
+            }
+        }
+        finalChange()
+    }, [passwordRequest])
 
     return (
         <div>
             <div className="passwordrecover">
                 <input value={newpassword} placeholder='Nova senha' onChange={e => alterpassword(e.target.value)}></input>
-                <button onClick={finalChange}>Alterar senha</button>
+                <button onClick={() => reqPas(true)}>Alterar senha</button>
             </div>
         </div>
     )
