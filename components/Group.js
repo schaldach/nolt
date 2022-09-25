cb38b706bf2bdd52c63dfe54aaf8d925129ccece
schaldach <gabimorgado0311@gmail.com>
2022-09-25 17:07:34 -0300

    hover nos botoes dos grupos

--- components/Group.js
+++ components/Group.js
@@ -68,13 +68,13 @@ function Group({allNotes, allLists, allLinks, allImages, notes, lists, links, im
                 <div className={!editMode?"":'displaynone'}>{title}</div>
                 <input className={editMode?"titleinput":'displaynone'} placeholder='Nome do grupo' 
                 value={title} onChange={e => onEdit(group, e.target.value)}></input>
-                <button onClick={() => setEdit(!editMode)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" className='dropdownsvg' fill="none" viewBox="0 0 24 24" stroke={editMode?'var(--color4)':'var(--color1)'} strokeWidth={2}>
+                <button onClick={() => setEdit(!editMode)} className={editMode?'':'idlefunction'}>
+                    <svg xmlns="http://www.w3.org/2000/svg" className='dropdownsvg' fill="none" viewBox="0 0 24 24" stroke='currentColor' strokeWidth={2}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                     </svg>
                 </button>
-                <button onClick={onFavorite}>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="dropdownsvg" fill='none' viewBox="0 0 24 24" stroke={favorite?'var(--color4)':'var(--color1)'} strokeWidth={2}>
+                <button onClick={onFavorite} className={favorite?'':'idlefunction'}>
+                    <svg xmlns="http://www.w3.org/2000/svg" className='dropdownsvg' fill='none' viewBox="0 0 24 24" stroke='currentColor' strokeWidth={2}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
                     </svg>
                 </button>
