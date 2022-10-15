d2521601eef878234ea5fc2757a790db4e036b41
schaldach <gabimorgado0311@gmail.com>
2022-10-14 22:05:25 -0300

    navbar com os nomes

--- components/NavBar.js
+++ components/NavBar.js
@@ -22,21 +22,25 @@ function NavBar({darkMode, setDarkMode, currentNote, changeCurrentNote, currentP
                     <svg xmlns="http://www.w3.org/2000/svg" className={currentPage==='home'?'navbarsvg':'displaynone'} viewBox="0 0 20 20" fill="currentColor">
                         <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
                     </svg>
+                    Home
                 </div>
                 </Link>
                 <div className="navbarwrapper">
                 <Link href={'/'+currentNote}>
                     <div className='contentselection' onClick={() => changeCurrentPage(currentNote)}>
                         <div className='navbar-select'>
-                        <div className={currentNote==='notas'?'':'displaynone'}>
+                        {currentNote==='notas' && 
+                        <>
                             <svg xmlns="http://www.w3.org/2000/svg" className={currentPage==='notas'?"navbarsvg":"displaynone"} viewBox="0 0 20 20" fill="currentColor">
                                 <path fillRule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z" clipRule="evenodd" />
                             </svg>
                             <svg xmlns="http://www.w3.org/2000/svg" className={currentPage!=='notas'?"navbarsvg":"displaynone"} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                             </svg>
-                        </div>
-                        <div className={currentNote==='listas'?'':'displaynone'}>
+                        </>
+                        }
+                        {currentNote==='listas' && 
+                        <>
                             <svg xmlns="http://www.w3.org/2000/svg" className={currentPage==='listas'?"navbarsvg":"displaynone"} viewBox="0 0 20 20" fill="currentColor">
                                 <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
                                 <path fillRule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clipRule="evenodd" />
@@ -44,8 +48,10 @@ function NavBar({darkMode, setDarkMode, currentNote, changeCurrentNote, currentP
                             <svg xmlns="http://www.w3.org/2000/svg" className={currentPage!=='listas'?"navbarsvg":"displaynone"} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
                             </svg>
-                        </div>
-                        <div className={currentNote==='links'?'':'displaynone'}>
+                        </>
+                        }
+                        {currentNote==='links' &&
+                        <>
                             <svg xmlns="http://www.w3.org/2000/svg" className={currentPage==='links'?"navbarsvg":"displaynone"} viewBox="0 0 20 20" fill="currentColor">
                                 <path d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2h-1.528A6 6 0 004 9.528V4z" />
                                 <path fillRule="evenodd" d="M8 10a4 4 0 00-3.446 6.032l-1.261 1.26a1 1 0 101.414 1.415l1.261-1.261A4 4 0 108 10zm-2 4a2 2 0 114 0 2 2 0 01-4 0z" clipRule="evenodd" />
@@ -53,15 +59,19 @@ function NavBar({darkMode, setDarkMode, currentNote, changeCurrentNote, currentP
                             <svg xmlns="http://www.w3.org/2000/svg" className={currentPage!=='links'?"navbarsvg":"displaynone"} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M10 21h7a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v11m0 5l4.879-4.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242z" />
                             </svg>
-                        </div>
-                        <div className={currentNote==='fotos'?'':'displaynone'}>
+                        </>
+                        }
+                        {currentNote==='fotos' &&
+                        <>
                             <svg xmlns="http://www.w3.org/2000/svg" className={currentPage==='fotos'?"navbarsvg":"displaynone"} viewBox="0 0 20 20" fill="currentColor">
                                 <path fillRule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clipRule="evenodd" />
                             </svg>
                             <svg xmlns="http://www.w3.org/2000/svg" className={currentPage!=='fotos'?"navbarsvg":"displaynone"} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                             </svg>
-                        </div>
+                        </>
+                        }
+                        {currentNote[0].toUpperCase() + currentNote.substring(1)}
                         <svg xmlns="http://www.w3.org/2000/svg" className='absolutesvg' fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                         </svg>
@@ -87,6 +97,7 @@ function NavBar({darkMode, setDarkMode, currentNote, changeCurrentNote, currentP
                     <svg xmlns="http://www.w3.org/2000/svg" className={currentPage!=='grupos'?'navbarsvg':'displaynone'} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4" />
                     </svg>
+                    Grupos
                 </div>
                 </Link>
                 <Link href='/profile'>
@@ -94,6 +105,7 @@ function NavBar({darkMode, setDarkMode, currentNote, changeCurrentNote, currentP
                     <div className={currentPage==='profile'?"profilepicwrapper whiteborder":'profilepicwrapper'}>
                     {profileUser.avatar_url?<img className="imgpicture" src={`https://uvvzrlvaqkcqmzdblein.supabase.co/storage/v1/object/public/${user.avatar_url}`}/>:<img className="navbarsvg" src={"https://uvvzrlvaqkcqmzdblein.supabase.co/storage/v1/object/public/avatars/user_placeholder.png"}/>}
                     </div>
+                    Perfil
                 </div>
                 </Link>
             </div>
